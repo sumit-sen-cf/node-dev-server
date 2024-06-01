8c97b01c3807ca31b88dc459c790b7402e083a4f
Ankita-456 <ankigupta1254@gmail.com>
2024-06-01 15:56:07 +0530

    salesServiceMaster api done.

--- models/Sales/salesServiceMasterModel.js
+++ models/Sales/salesServiceMasterModel.js
@@ -13,55 +13,67 @@ const salesServiceMaster = new Schema({
     },
     amount_status: {
         type: String,
-        enu: ["calculated", "input"],
+        enum: ["calculated", "input"],
     },
     is_excel_upload: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     no_of_hours_status: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     goal_status: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     day_status: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     quantity_status: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     brand_name_status: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     hashtag: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     indiviual_amount_status: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     start_end_date_status: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     per_month_amount_status: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     no_of_creators: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     deliverables_info: {
-        type: String,
-        required: false
+        type: Boolean,
+        required: false,
+        default: false,
     },
     remarks: {
         type: String,
