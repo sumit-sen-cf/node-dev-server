b3bd3973b954e6ce19ef390d621ada40c152af64
abhi4pr <ascs739@gmail.com>
2024-06-05 13:26:46 +0530

    working on camp plan/phase apis

--- controllers/opCampaignPhase.js
+++ controllers/opCampaignPhase.js
@@ -1,6 +1,6 @@
 const response = require("../common/response.js");
 const opCampaignPhaseModel = require("../models/opCampaignPhaseModel.js");
-
+const opCampaignPlanModel = require("../models/opCampaignPlanModel.js");
 
 exports.addCampaignPhase = async (req, res) => {
 
@@ -49,6 +49,9 @@ exports.getOpCampaignPhases = async (req, res) => {
     try {
         const campaignPhases = await opCampaignPhaseModel
             .aggregate([
+                {
+                    $match: { campaignId: req.params.id }
+                },
                 {
                     $lookup: {
                         from: "opcampaignmodels",
@@ -100,8 +103,67 @@ exports.getOpCampaignPhases = async (req, res) => {
         if (!campaignPhases) {
             return response.returnFalse(200, req, res, "No Reord Found...", []);
         }
-        res.status(200).send(campaignPhases)
+
+        const inventoryDataListPre = await axios.get(
+            'https://purchase.creativefuel.io/webservices/RestController.php?view=inventoryDataList'
+        );
+        const inventoryDataList = inventoryDataListPre.data.body;
+
+        const enrichedCampaignPhases = campaignPhases.map(phase => {
+            const inventoryData = inventoryDataList.find(item => item.p_id === phase.p_id) || {};
+            return {
+                ...phase,
+                page_name: inventoryData.page_name || null,
+                follower_count: inventoryData.follower_count || null,
+                page_link: inventoryData.page_link || null,
+                cat_name: inventoryData.cat_name || null
+            };
+        });
+
+        res.status(200).send(enrichedCampaignPhases)
     } catch (err) {
         return response.returnFalse(500, req, res, err.message, {});
     }
 };
+
+exports.deleteCampaignPhaseDataByCampaignId = async (req, res) => {
+    opCampaignPhaseModel.deleteMany({ campaignId: req.params.id }).then(item => {
+        if (item) {
+            return res.status(200).json({ success: true, message: 'Campaign Phase Data deleted By Campaign Id' })
+        } else {
+            return res.status(404).json({ success: false, message: 'Campaign Phase Data by this Campaign Id not found' })
+        }
+    }).catch(err => {
+        return res.status(400).json({ success: false, message: err.message })
+    })
+};
+
+exports.deleteCampaignPhase = async (req, res) => {
+    opCampaignPhaseModel.deleteOne({ _id: req.params.id }).then(item => {
+        if (item) {
+            return res.status(200).json({ success: true, message: 'Campaign Phase Data deleted' })
+        } else {
+            return res.status(404).json({ success: false, message: 'Campaign Phase Data not found' })
+        }
+    }).catch(err => {
+        return res.status(400).json({ success: false, message: err.message })
+    })
+};
+
+exports.replacePhasePage = async (req, res, next) => {
+    const id = req.body._id;
+
+    const result = await opCampaignPlanModel.findByIdAndUpdate(req.body._id,
+        { 
+            p_id: req.body.new_pid
+        }, 
+        { new: true }
+    );
+
+    const result2 = await opCampaignPhaseModel.findOneAndUpdate({ campaignId: id, p_id: req.body.old_pid }, 
+    {
+        p_id: req.body.new_pid
+    })
+
+    res.status(200).json({ data: result })
+}
