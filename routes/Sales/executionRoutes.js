dc14d43e2c711c9bd004d84cbac9fc76ea8dd6ea
Ankita-456 <ankigupta1254@gmail.com>
2024-06-15 13:08:31 +0530

    execution create add updateStatus api.

--- routes/Sales/executionRoutes.js
+++ routes/Sales/executionRoutes.js
@@ -10,5 +10,6 @@ router.get("/sales/sales_booking_execution/:id", verifyToken, executionControlle
 router.put("/sales/sales_booking_execution/:id", verifyToken, executionController.updateExecutionDetial);
 router.get("/sales/sales_booking_execution", verifyToken, executionController.getExcutionList);
 router.delete("/sales/sales_booking_execution/:id", verifyToken, executionController.deleteExecution);
+router.put("/sales/execution_status/:id", verifyToken, executionController.updateStatusExecution);
 
 module.exports = router;
