314ac3de79f6327eef0830a6382db7a4ab88a7d8
Aman Rathod <amanrathod197@gmail.com>
2024-07-12 15:21:33 +0530

    sale booking id and account master id default 2k to starting set.

--- models/accounts/accountMasterModel.js
+++ models/accounts/accountMasterModel.js
@@ -77,7 +77,7 @@ accountMasterSchema.pre('save', async function (next) {
         if (lastAgency && lastAgency.account_id) {
             this.account_id = lastAgency.account_id + 1;
         } else {
-            this.account_id = 1;
+            this.account_id = 2000;
         }
     }
     next();
