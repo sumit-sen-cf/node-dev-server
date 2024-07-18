f5da87fb7cefad6a62c663e4bbc651a521f8412c
abhi4pr <ascs739@gmail.com>
2024-07-18 11:44:05 +0530

    Update variables.js

--- variables.js
+++ variables.js
@@ -10,5 +10,6 @@ module.exports = {
     IMAGE_URL: 'https://storage.googleapis.com/jarvis-dev-bucket/',
     // IMAGE_URL: 'https://storage.googleapis.com/node-prod-bucket/',
     ALLOWED_URL: 'http://34.70.225.161 ',
-    // ALLOWED_URL: 'https://www.jarvis.work'
+    // ALLOWED_URL: 'https://www.jarvis.work',
+    CICD: 'testing'
 };
