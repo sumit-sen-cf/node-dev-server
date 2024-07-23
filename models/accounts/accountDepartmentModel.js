2e36004673ec45c47f5f9ddb5b2c13a2bd0ca4f7
Aman rathod <amanrathod197@gmail.com>
2024-07-23 18:35:04 +0530

    department master crud create POST,PUT,GET,DELETE

---
+++ models/accounts/accountDepartmentModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const constant = require("../../common/constant");
+
+const accountDepartmentSchema = new mongoose.Schema({
+    department_name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    created_by: {
+        type: Number,
+        required: false,
+    },
+    updated_by: {
+        type: Number,
+        required: false,
+    },
+    status: {
+        type: Number,
+        required: false,
+        default: constant?.ACTIVE,
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("accountDepartmentModel", accountDepartmentSchema);
